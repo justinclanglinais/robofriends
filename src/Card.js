18a137f28710f3bf63e6e7b88e6d7b7ba3f9d381
Justin Langlinais <justinclanglinais@gmail.com>
2023-12-05 15:22:05 -0800

    building card

---
+++ src/Card.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const card = () => {
+    return (
+        <div>
+            <img src="https://robohash.org/test">
+
+            </img>
+            <div>
+                <h2>
+                    Name
+                </h2>
+            </div>
+            <div>
+                <p>
+                    Email
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default card;
