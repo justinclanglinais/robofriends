72576ab03985ffba84eabe0582ba8559f6127db9
Justin Langlinais <justinclanglinais@gmail.com>
2023-12-06 09:47:10 -0800

    add fetch

--- src/App.js
+++ src/App.js
@@ -7,22 +7,36 @@ class App extends Component {
     constructor () {
         super ()
         this.state = {
-            robots: robots,
+            robots: [],
             searchField: ''        
         }
     }
+
+    componentDidMount() {   
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then((response => response.json()))
+            // .then(users => {})
+            .then(users => this.setState({ robots }))
+    }
+
     onSearchChange = (event) => {
         this.setState({searchField: event.target.value})
     }
+
     render () {
         const filteredRobots = this.state.robots.filter(robot => {
             return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
         })
+        if (this.state.robots.length === 0) {
+            return <h1>Loading...</h1>
+        }
         return (
             <div className='tc'>
                 <h1 className='f1'>ROBOFRIENDS</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={filteredRobots} />
+                <scroll>
+                    <CardList robots={filteredRobots} />
+                </scroll>
             </div>
         )
     }
