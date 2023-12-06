3a7db0ed3a5168413612b038bdad7cf362daf335
Justin Langlinais <justinclanglinais@gmail.com>
2023-12-05 18:16:21 -0800

    add props to card

--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import 'tachyons';
 
-const card = () => {
+const card = (props) => {
     return (
-        <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+        <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5" >
             <img src="https://robohash.org/test" />
             <div>
-                <h2>Name</h2>
-                <p>Email</p>
+                <h2>{props.name}</h2>
+                <p>{props.email}</p>
             </div>
         </div>
     )
