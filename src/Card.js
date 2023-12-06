d4a747bd856b39ed91b822a513d09186398a1de1
Justin Langlinais <justinclanglinais@gmail.com>
2023-12-05 18:33:07 -0800

    map robots

--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import 'tachyons';
 
-const card = (props) => {
+const card = ({id, name, email}) => {
     return (
         <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5" >
-            <img alt='robots' src={`https://robohash.org/${props.id}`} />
+            <img alt='robots' src={`https://robohash.org/${id}`} />
             <div>
-                <h2>{props.name}</h2>
-                <p>{props.email}</p>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
     )
--- src/index.js
+++ src/index.js
@@ -7,12 +7,15 @@ import 'tachyons';
 import {robots} from './robots'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const cards = robots.map(bot => 
+  <div>
+    <Card id={bot.id} name={bot.name} email={bot.email} />
+  </div>
+)
 root.render(
   <React.StrictMode>
     <div>
-      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-      <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
-      <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
+      {cards}
     </div>
   </React.StrictMode>
 );
