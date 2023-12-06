86ae920fca71b387910d57db3304544a8574fcb2
Justin Langlinais <justinclanglinais@gmail.com>
2023-12-05 21:50:58 -0800

    add state and convert App to class

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
 import CardList from './CardList';
 import {robots} from './robots';
 import SearchBox from './SearchBox';
 
+const state = {
+    robots: robots,
+    searchField: ''
+}
 
-const App = () => {
-    return (
-        <div>
-            <h1>Robo Friends</h1>
-            <SearchBox />
-            <CardList robots={robots} />
-        </div>
-    )
+class App extends React.Component {
+    render () {
+        return (
+            <div className='tc'>
+                <h1>Robo Friends</h1>
+                <SearchBox />
+                <CardList robots={robots} />
+            </div>
+        )
+    }
+    
 }
 
 export default App;
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 const SearchBox = () => {
     return (
-        <div>
-            
+        <div className='pa2'>
+            <input 
+                className='pa3 ba b--green bg-lightest-blue'
+                type='search' 
+                placeholder='search robots' 
+            />
         </div>
     )
 }
