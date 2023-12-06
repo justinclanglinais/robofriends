66000bf261c0b1a69ec0376ae2f5fee2b58decd2
Justin Langlinais <justinclanglinais@gmail.com>
2023-12-05 22:03:55 -0800

    adding filtering

--- src/App.js
+++ src/App.js
@@ -3,18 +3,27 @@ import CardList from './CardList';
 import {robots} from './robots';
 import SearchBox from './SearchBox';
 
-const state = {
-    robots: robots,
-    searchField: ''
-}
-
 class App extends React.Component {
+    constructor () {
+        super ()
+        this.state = {
+            robots: robots,
+            searchField: ''        
+        }
+    }
+    onSearchChange = (event) => {
+        this.setState({searchField: event.target.value})
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+        })
+        console.log(filteredRobots)
+    }
     render () {
         return (
             <div className='tc'>
                 <h1>Robo Friends</h1>
-                <SearchBox />
-                <CardList robots={robots} />
+                <SearchBox searchChange={this.onSearchChange}/>
+                <CardList robots={this.state.robots} />
             </div>
         )
     }
