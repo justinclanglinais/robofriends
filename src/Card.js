f2fe8228286d50035cd9bb61b98d40873fd1d8de
Justin Langlinais <justinclanglinais@gmail.com>
2023-12-05 18:18:24 -0800

    img props

--- src/Card.js
+++ src/Card.js
@@ -4,7 +4,7 @@ import 'tachyons';
 const card = (props) => {
     return (
         <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5" >
-            <img src="https://robohash.org/test" />
+            <img alt='robots' src={`https://robohash.org/${props.id}`} />
             <div>
                 <h2>{props.name}</h2>
                 <p>{props.email}</p>
